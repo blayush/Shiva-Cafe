76c618ec56967bbb5ba5f93c9fd26575033e3f67
Ayush Mishra <ayushmishra42@gmail.com>
2021-08-02 15:10:25 +0530

    try

--- app/src/main/java/com/example/boomshiva/splashScreen.java
+++ app/src/main/java/com/example/boomshiva/splashScreen.java
@@ -2,7 +2,9 @@ package com.example.boomshiva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class splashScreen extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread td=new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1800);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent intent=new Intent(splashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };td.start();
     }
 }
